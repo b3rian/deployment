a007d785b6b139ad8ff7c997431c805c031c3052
b3rian <brayann.8189@gmail.com>
2025-06-04 09:36:50 +0300

    commit

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 from fastapi import FastAPI
-from typing import Annotated
-from pydantic import BaseModel, Field, Query, HttpUrl
+import pandas as pd
+import joblib
+
+app = FastAPI() 
+model = joblib.load('house_prediction.pkl') # loading the model
+
+@app.get('/predict_price/')
+async def predict_price(bedrooms : int, bathrooms: int, sqft : int):
+    features = pd.DataFrame([[bedrooms, bathrooms, sqft]],
+                            columns=["bedrooms", "bathrooms", "sqft"])
+    price = model.predict(features)[0]
+    return{'predicted price' : price}
+
+
  
