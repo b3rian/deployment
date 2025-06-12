ce66c65534acbc014ae4cbb8b685a05acbb8674b
b3rian <brayann.8189@gmail.com>
2025-06-12 11:03:11 +0300

    main commits

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 from fastapi import FastAPI, Cookie, Header, HttpException, status, Request, BackgroundTasks
-import pandas as pd
-import joblib
-from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, Field
-import time
 from typing import Annotated, Optional
+import httpx
+import os
+from dotenv import load_dotenv
 
 app = FastAPI()
 
 from pydantic import BaseModel
 
-# models with pydantic
+
 class WeatherRequest(BaseModel):
     city: str
 
