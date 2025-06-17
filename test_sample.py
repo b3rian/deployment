37afa6b58763d0cefdc56bb336daddc8db70b495
b3rian <brayann.8189@gmail.com>
2025-06-17 10:43:05 +0300

    initial test commits

--- test_sample.py
+++ test_sample.py
@@ -1,9 +1,10 @@
 import pytest
 
-@pytest.fixture
-def sample_data():
-    return [1,2,3]
+@pytest.fixture(params=[1, 2, 3])
+def number(request):
+    return request.param
 
-def test_sum(sample_data):
-    assert sum(sample_data) == 6
+def test_is_even(number):
+    assert number % 2 == 0  # Will fail for 1 and 3
+
     
